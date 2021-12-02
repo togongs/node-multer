6d10815191ffdbf5ad3df49aedcf46bf1ce194e5
changwoo Jeong <jcw9828@gmail.com>
2021-12-02 13:29:53 +0900

    multer,s3 기본설정

---
+++ multer/s3.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const upload = multer({
+  storage: multerS3({
+    // 어디에 저장할지
+    s3: s3,
+    bucket: "",
+    acl: "",
+    key: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, Date.now() + ext);
+    },
+    limits: { fileSize: 5 * 1024 * 1024 },
+  }),
+});
+
+module.exports = upload;
