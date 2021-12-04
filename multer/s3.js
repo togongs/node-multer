02cb39b384ab3351f6aa9ef11598cb197581bfa7
changwoo Jeong <jcw9828@gmail.com>
2021-12-04 15:24:53 +0900

    readme 수정

--- multer/s3.js
+++ multer/s3.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
-const multerS3 = require("multer-s3");
-const aws = require("aws-sdk");
+const multerS3 = require("multer-s3"); // 이미지 업로드 시 S3에 업로드 하도록 만들어주는 모듈
+const aws = require("aws-sdk"); // AWS 서비스를 연결하기 위한 모듈
 const path = require("path");
 aws.config.loadFromPath(__dirname + "/awsconfig.json"); // sdk로딩
 const s3 = new aws.S3(); // s3객체
